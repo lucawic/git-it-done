203a5f79d438e31b1f399db76ddb7a1a31c01f67
lucawic <lucaswick@email.arizona.edu>
2021-07-17 16:42:06 -0700

    this is almost working it has the necessary buttons but I am still unable to display specific repo issues and have the auxillary buttons added in 6.5 to work

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector('#language-buttons');
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -19,6 +20,20 @@ var formSubmitHandler = function(event) {
   } else {
     alert('Please enter a GitHub username');
   }
+};
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+      displayRepos(data.items,language);
+      });
+    } else {
+      alert("Error: GitHub User Not Found");
+    }
+  });
 };
 
 var getUserRepos = function(user) {
@@ -56,7 +71,7 @@ var displayRepos = function(repos, searchTerm) {
   // loop over repos
   for (var i = 0; i < repos.length; i++) {
     // format repo name
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
 
     // create a container for each repo
     var repoEl = document.createElement("a");
@@ -64,7 +79,7 @@ var displayRepos = function(repos, searchTerm) {
     repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     // create a span element to hold repository name
-    var titleEl = document.createElement('span');
+    var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
 
     // append to container
@@ -90,5 +105,18 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+  console.log(language);
+
+  if (language) {
+    getFeaturedRepos(language);
+
+    //clear the old content
+    repoContainerEl.textContent = "";
+  }
+}
+
 // add event listeners to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
